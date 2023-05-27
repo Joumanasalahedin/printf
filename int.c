5382771e302f030fa8553cd3d1cb81017ecebaeb
Joumana Arafa <joumanasalahedin@gmail.com>
2023-05-28 00:24:01 +0200

    betty fix for int.c

--- int.c
+++ int.c
@@ -3,7 +3,7 @@
 /**
   * print_int - prints integers and decimals
   * @args: arguments
-  * Returns: number of characters printed
+  * Return: number of characters printed
   */
 
 int print_int(va_list args)
@@ -20,25 +20,21 @@ int print_int(va_list args)
 		num = -num;
 		chars++;
 	}
-
 	temp = num;
-
 	if (num == 0)
 	{
 		_putchar(48);
 		return (chars);
 	}
-
 	while (temp != 0)
 	{
 		temp /= 10;
 		digits++;
 	}
 
-	arr = (char*)malloc(digits * sizeof(char));
+	arr = (char *)malloc(digits * sizeof(char));
 	if (arr == NULL)
 		return (chars);
-
 	while (num != 0)
 	{
 		arr[index++] = 48 + (num % 10);
@@ -50,7 +46,6 @@ int print_int(va_list args)
 		_putchar(arr[i]);
 		chars++;
 	}
-
 	free(arr);
 	return (chars);
 }
